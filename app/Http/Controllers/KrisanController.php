f43714b3345468d5a66ef0daf307c4d586f23013
Ofren Dialsa <153308025+OfrenDialsa@users.noreply.github.com>
2024-10-07 17:39:21 +0700

    tes

--- app/Http/Controllers/KrisanController.php
+++ app/Http/Controllers/KrisanController.php
@@ -34,21 +34,16 @@ class KrisanController extends Controller
      */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'judul' => 'required|string',
-            'aduan' => 'required|string',
-        ]);
-    
+        $input = $request->all();
         // Create a new Krisan instance
         $krisan = new Krisan();
-        $krisan->judul = $validatedData['judul'];
-        $krisan->aduan = $validatedData['aduan'];
+        $krisan = $input;
         $krisan->save();
     
         // Redirect to the index page
         return redirect()->route('krisan.index')>with('flash_message', 'Kritik dan saran telah ditambahkan!');
 
-        //$input = $request->all();
+        //
         
         //Krisan::create($input);
         //return redirect('krisan')->with('flash_message', 'Kritik dan saran telah ditambahkan!');
